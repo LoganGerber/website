ce07090accee882e4a7ecc2ceb557649c74ae67f
LoganGerber <logangerber1014@yahoo.com>
2022-03-13 23:43:57 -0700

    Begin work on page header

---
+++ src/MainSite.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import PageHeader from '@/components/PageHeader.vue';
+</script>
+
+<template>
+	<PageHeader />
+	<RouterView />
+</template>
+
+<style>
+@import "@/assets/loganBase.css";
+@import "@/assets/dualGrid.css";
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+	<nav id="pageHeader">
+		<RouterLink to="/" id="homeLink">Logan Gerber</RouterLink>
+		<!-- <RouterLink to="/resume" id="resumeLink"></RouterLink> -->
+		<a href="https://github.com/LoganGerber" id="githubLink">
+			<font-awesome-icon :icon="['fab', 'github-square']" inverse title="Github" />
+		</a>
+		<a href="mailto:logangerber@protonmail.com" id="emailLink">
+			<font-awesome-icon icon="envelope" inverse title="Email" />
+		</a>
+		<!-- <RouterLink to="/blog" id="blogLink"></RouterLink> -->
+	</nav>
+</template>
+
+<style scoped>
+#pageHeader {
+	display: flex;
+	flex-direction: row;
+
+	justify-content: flex-start;
+	flex-wrap: nowrap;
+	align-items: center;
+
+	border: 1px solid white;
+	height: 64px;
+}
+
+#homeLink {
+	color: white;
+	text-decoration: none;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 30pt;
+	font-weight: 700;
+	padding-left: 10px;
+}
+
+/* #resumeLink {
+} */
+
+#githubLink {
+}
+
+#emailLink {
+}
+
+#blogLink {
+}
+
+#hamburgerMenu {
+}
+</style>
