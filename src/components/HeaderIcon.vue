f372fbeb13c681e2056c024b33b6968f51dd52a5
LoganGerber <logangerber1014@yahoo.com>
2022-04-01 01:06:31 -0700

    Create HeaderIcon component

---
+++ src/components/HeaderIcon.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+defineProps({
+	href: {
+		type: String,
+		required: true
+	},
+	target: {
+		type: String,
+		default: '_blank'
+	},
+	icon: {
+		type: [String, Array],
+		required: true
+	},
+	title: String,
+	iconSize: {
+		type: Number,
+		default: 12
+	},
+	captionSize: {
+		type: Number,
+		default: 12
+	}
+});
+</script>
+
+<template>
+	<a :href="href" :target="target" class="headerIcon">
+		<font-awesome-icon :icon="icon" inverse :title="title" :style="{ 'fontSize': iconSize + 'pt' }" />
+		<span class="iconCaption" :style="{ 'fontSize': captionSize + 'pt' }">
+			<slot></slot>
+		</span>
+	</a>
+</template>
+
+<style scoped>
+.headerIcon {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	text-decoration: none;
+}
+
+.iconCaption {
+	color: white;
+	font-weight: bold;
+}
+</style>
