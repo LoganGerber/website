24e6ed753c9a03bc1add356f9e6abfe7ce68d2c3
LoganGerber <logangerber1014@yahoo.com>
2022-04-27 01:41:57 -0700

    Remove header icon caption from flow and center under icon

--- src/components/HeaderIcon.vue
+++ src/components/HeaderIcon.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { ref, computed, onMounted, onUpdated } from 'vue';
 
 defineProps({
 	href: {
@@ -17,21 +18,48 @@ defineProps({
 	title: String,
 	iconSize: {
 		type: Number,
-		default: 12
+		default: 28
 	},
 	captionSize: {
 		type: Number,
-		default: 12
+		default: 15
 	}
 });
 
 let isHovered = ref(false);
+const caption = ref<HTMLSpanElement | null>(null);
+const parent = ref<HTMLAnchorElement | null>(null);
+
+const captionOpacity = computed(() => {
+	return isHovered.value ? '1' : '0';
+});
+
+onMounted(() => { updateCaptionOffset() });
+onUpdated(() => { updateCaptionOffset() });
+
+function updateCaptionOffset() {
+	let parentWidth = parent.value?.clientWidth;
+	let captionWidth = caption.value?.clientWidth;
+
+	if (parentWidth === undefined || captionWidth === undefined) {
+		return;
+	}
+
+	let marginLeft = -1 * (captionWidth - parentWidth) / 2;
+
+	console.log(marginLeft);
+	(caption.value as HTMLSpanElement).style.marginLeft = marginLeft.toString() + 'px';
+}
+
 </script>
 
 <template>
-	<a :href="href" :target="target" class="headerIcon" @mouseover="isHovered = true" @mouseout="isHovered = false">
+	<a :href="href" :target="target" class="headerIcon" @mouseover="isHovered = true" @mouseout="isHovered = false"
+		ref="parent">
 		<font-awesome-icon :icon="icon" inverse :title="title" :style="{ 'fontSize': iconSize + 'pt' }" />
-		<span class="iconCaption" :style="{ 'fontSize': captionSize + 'pt', 'opacity': isHovered ? '1' : '0' }">
+		<span class="iconCaption" :style="{
+			'fontSize': captionSize + 'pt', 'opacity': captionOpacity
+		}" ref="caption">
 			<slot></slot>
 		</span>
 	</a>
@@ -39,6 +67,8 @@ let isHovered = ref(false);
 
 <style scoped>
 .headerIcon {
+	position: relative;
+
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
@@ -49,8 +79,8 @@ let isHovered = ref(false);
 .iconCaption {
 	color: white;
 	font-weight: bold;
+	position: absolute;
 
-	position: relative;
-	height: 0;
+	top: calc(100% + 10px);
 }
 </style>
